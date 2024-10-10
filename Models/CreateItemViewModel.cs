97eeed572cd338fb8da8ef768e8b0ad815dabfce
Omen <Omen@PC>
2024-10-10 22:11:00 +0300

    Add project files.

---
+++ Models/CreateItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace WebApplication2.Models
+{
+    public class CreateItemViewModel
+    {
+        public Item Item { get; set; } = new Item(); 
+        public string ItemType { get; set; } = string.Empty; 
+        public List<SelectListItem> ItemTypes { get; set; } = new List<SelectListItem>(); S
+    }
+}
